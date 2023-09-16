cec976640c282d90355d5f0f09f098669e9a8180
LSX-s-Software <2682191498@qq.com>
2023-09-16 22:05:51 +0800

    feat: 添加SeBS的dynamic-html

---
+++ functions/dynamic-html/handler.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import json
+from random import sample
+from os import path
+from time import time
+
+from jinja2 import Template
+
+# {
+#     "username": "Tsinghua University",
+#     "random_len": 1000
+# }
+
+SCRIPT_DIR = path.abspath(path.join(path.dirname(__file__)))
+
+def handle(event):
+    event = json.loads(event)
+    name = event.get('username')
+    size = event.get('random_len')
+
+    start = time()
+    cur_time = datetime.now()
+    random_numbers = sample(range(0, 1000000), size)
+    template = Template(open(path.join(SCRIPT_DIR, 'templates', 'template.html'), 'r').read())
+    html = template.render(username = name, cur_time = cur_time, random_numbers = random_numbers)
+
+    latency = time() - start
+    return {'latency': latency, 'data': html}
--- functions/pyaes/handler.py
+++ functions/pyaes/handler.py
@@ -36,4 +36,4 @@ def handle(event):
 
     latency = time() - start
 
-    return {'latency': latency}
+    return {'latency': latency, 'data': ''}
