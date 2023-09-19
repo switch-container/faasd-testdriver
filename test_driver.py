b0197c1471484bb939389ef9abe4ae2b42135653
LSX-s-Software <2682191498@qq.com>
2023-09-19 20:11:28 +0800

    feat: 显示进度条、最终结果以表格形式展示

--- test_driver.py
+++ test_driver.py
@@ -4,6 +4,8 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 import json
 from time import time
+from tqdm import tqdm
+import tabulate
 
 if __name__ == '__main__':
     # 初始化requests
@@ -30,53 +32,43 @@ if __name__ == '__main__':
     max_retry = config.get('max_retry', 3)
     average = config.get('average', 3)
     warm_up_count = config.get('warm_up_count', 3)
+    
+    result = []
 
     # 测试函数
-    for function, conf in functions.items():
-        print(f'Testing {function}')
+    for function, conf in tqdm(functions.items(), desc='Testing Functions', unit='function', position=0, ncols=80, leave=None, bar_format = "{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}]"):
         request_body = conf.get('request_body')
 
         # 预热
-        print(f'\tWarming up...')
-        for i in range(warm_up_count):
-            try:
-                http.post(f'{gateway}/function/{function}', json=request_body, timeout=timeout)
-            except Exception as e:
-                print(f'\tError: {e}')
-                continue
-        print(f'\tWarm up completed')
+        for i in tqdm(range(warm_up_count), desc=f'Warming up {function}', unit='warmup', position=1, ncols=80, leave=None):
+            http.post(f'{gateway}/function/{function}', json=request_body, timeout=timeout)
 
         # 测试
         total_latency = 0
         total_e2e_latency = 0
-        for i in range(average):
+        for i in tqdm(range(average), desc=f'Testing {function}', unit='test', position=1, ncols=80, leave=None):
             start = time()
-            try:
-                response = http.post(f'{gateway}/function/{function}', json=request_body, timeout=timeout)
-            except Exception as e:
-                print(f'\tError: {e}')
-                continue
+            response = http.post(f'{gateway}/function/{function}', json=request_body, timeout=timeout)
             e2e_latency = time() - start
             if response.status_code != 200:
-                print(f'\tError: [{response.status_code} {response.reason}] {response.text}')
-                continue
+                raise f'Error: [{response.status_code} {response.reason}] {response.text}'
             if response.text is None or response.text == '':
-                print(f'\tError: Empty response')
-                continue
+                raise 'Error: Empty response'
             start = response.text.find('{')
             end = response.text.rfind('}')
             if start == -1 or end == -1:
-                print(f'\tError: Invalid response')
-                continue
+                raise 'Error: Invalid response'
             data = json.loads(response.text[start:end+1])
             latency = data.get('latency')
             if latency is None:
-                print(f'\tError: Invalid response')
-                continue
+                raise 'Error: Invalid response'
             total_latency += latency
             total_e2e_latency += e2e_latency
-            print(f'\tTest {i+1}: latency: {latency}s, end-to-end latency: {e2e_latency}s')
-        print(f'\tAverage Latency: {total_latency / average}s')
-        print(f'\tAverage End-to-end Latency: {total_e2e_latency / average}s')
+        result.append({
+            'Name': function,
+            'Average Latency(ms)': int(total_latency * 1000 / average),
+            'Average E2E Latency(ms)': int(total_e2e_latency * 1000 / average)
+        })
     
     print('Test completed')
+    print(tabulate.tabulate(result, headers='keys', floatfmt='.3f', numalign='right'))
