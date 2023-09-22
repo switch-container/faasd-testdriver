c42c454debce9c6e843036bbce533351da631e08
LSX-s-Software <2682191498@qq.com>
2023-09-22 18:29:20 +0800

    fix: incorrect faas-cli checking method

--- test_driver.py
+++ test_driver.py
@@ -12,9 +12,9 @@ class TestDriver:
     def __init__(self, gateway: str):
         self.gateway = gateway
 
-        # 检查faas-cli
+        # Check faas-cli
         try:
-            subprocess.check_call(['faas-cli', 'version'])
+            subprocess.check_call(['which', 'faas-cli'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         except:
             choice = input('faas-cli not found, do you want to install it? [Y/n] ')
             if choice == '' or choice.lower() == 'y':
@@ -22,36 +22,37 @@ class TestDriver:
             else:
                 print('Error: faas-cli not found')
                 exit(1)
+        subprocess.call(['faas-cli', 'version'])
 
     def login(self, username: str, password: str):
-        '''登录faas-cli'''
+        '''Login faas-cli'''
         subprocess.check_call(['faas-cli', 'login', '-g', self.gateway, '-u', username, '-p', password])
 
     def logout(self):
-        '''登出faas-cli'''
+        '''Logout faas-cli'''
         subprocess.check_call(['faas-cli', 'logout', '-g', self.gateway])
 
     def build(self, parallel: int):
-        '''构建函数镜像'''
+        '''Build function images'''
         subprocess.check_call(['faas-cli', 'build', '-f', 'functions.yml', '--parallel', str(parallel)], cwd='functions')
 
     def push(self, parallel: int):
-        '''推送函数镜像'''
+        '''Push function images'''
         subprocess.check_call(['faas-cli', 'push', '-f', 'functions.yml', '--parallel', str(parallel)], cwd='functions')
 
     def deploy(self):
-        '''部署函数镜像'''
+        '''Deploy functions'''
         subprocess.check_call(['faas-cli', 'deploy', '-f', 'functions.yml', '-g', self.gateway], cwd='functions')
 
     def up(self, parallel: int):
-        '''构建、推送、部署函数镜像'''
+        '''Build, push and deploy functions'''
         self.build(parallel)
         self.push(parallel)
         self.deploy()
 
     def test(self, functions: dict, timeout: int, max_retry: int, average: int, warm_up_count: int):
-        '''测试函数'''
-        # 初始化requests
+        '''Test functions'''
+        # Init requests
         retry_strategy = Retry(
             total=max_retry,
             status_forcelist=[429, 500, 502, 503, 504],
@@ -67,11 +68,11 @@ class TestDriver:
         for function, conf in tqdm(functions.items(), desc='Testing Functions', unit='function', position=0, ncols=80, leave=None, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}]'):
             request_body = conf.get('request_body')
 
-            # 预热
+            # Warm up
             for _ in tqdm(range(warm_up_count), desc=f'Warming up {function}', unit='warmup', position=1, ncols=80, leave=None):
                 http.post(f'{self.gateway}/function/{function}', json=request_body, timeout=timeout)
 
-            # 测试
+            # Perform test
             total_latency = 0
             total_e2e_latency = 0
             for _ in tqdm(range(average), desc=f'Testing {function}', unit='test', position=1, ncols=80, leave=None):
@@ -114,36 +115,37 @@ class TestDriver:
         print('Test completed')
         print(tabulate.tabulate(result, headers='keys', floatfmt='.3f', numalign='right'))
 
-        # 绘制柱状图
+        # Draw result
         self.draw_result(result)
 
         return result
     
     @staticmethod
     def draw_result(data: list[dict]):
-        # 提取函数名和对应的两个指标值
+        '''Draw test result'''
+        # Prepare data
         names = [item['Name'] for item in data]
         avg_latency = [item['Average Latency(ms)'] for item in data]
         avg_other_latency = [item['Average Other Latencies(ms)'] for item in data]
 
-        # 设置柱状图的位置
+        # Set x axis range
         x = range(len(names))
 
-        # 绘制柱状图
+        # Draw bar chart
         plt.bar(x, avg_latency, width=0.4, label='Average Computing Latency (ms)')
         plt.bar(x, avg_other_latency, width=0.4, label='Average Other Latencies (ms)', bottom=avg_latency)
 
-        # 添加标签和标题
+        # Set title, x axis label and y axis label
         plt.title('Latency Comparison')
         plt.xlabel('Function Name')
         plt.ylabel('Latency (ms)')
 
-        # 设置x轴刻度标签
+        # Set x axis scale
         plt.xticks(x, names, rotation=30)
 
-        # 添加图例
+        # Set legend
         plt.legend()
 
-        # 显示图形
+        # Show plot
         plt.subplots_adjust(bottom=0.25)
         plt.show()
